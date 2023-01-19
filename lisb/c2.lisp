fc91c3ac1cc974fd87e805a48285e95c4f5585cf
gardrek <gardrek.the.destroyer@gmail.com>
2023-01-19 06:22:34 +0000

    cleaned up and quarantined junk

--- lisb/c2.lisp
+++ lisb/c2.lisp
@@ -1,12 +1,5 @@
-(if (truthy
-(def cons2 (fn (x y) (fn (f) (f x y)))))
-never
-
-(if (truthy
-(def cdr2 (fn (c) (c (fn (x y) (id y))))))
-never
-
-(def car2 (fn (c) (c (fn (x y) (id x)))))
-)))
-
-
+(
+    (cons2 (fn (x y) (fn (f) (f x y))))
+    (cdr2 (fn (c) (c (fn (x y) (id y)))))
+    (car2 (fn (c) (c (fn (x y) (id x)))))
+)
